8302830bd0a98a0b7f9ea905716de22726b7cee7
AND <andrey_g.pt@mail.ru>
2018-02-16 18:01:09 +0200

    logout

--- javascript.js
+++ javascript.js
@@ -18,6 +18,17 @@ image.onload = function()
   context.drawImage(image, 64, 32)
 }
 
-function O(i) { return typeof i == 'object' ? i : document.getElementById(i) }
-function S(i) { return O(i).style                                            }
-function C(i) { return document.getElementsByClassName(i)                    }
+function O(i) 
+{ 
+	return typeof i == 'object' ? i : document.getElementById(i) 
+}
+
+function S(i) 
+{ 
+	return O(i).style                                            
+}
+
+function C(i) 
+{ 
+	return document.getElementsByClassName(i) 
+}
